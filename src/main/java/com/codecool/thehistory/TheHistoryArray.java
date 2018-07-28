d092189f26aee26325f72ef2f763ad82f96ea0c3
ferencDobi <ferencdbi@gmail.com>
2018-07-28 13:15:55 +0200

    insertion replace refactored to perform better

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -56,9 +56,8 @@ public class TheHistoryArray implements TheHistory {
         removeWord("");
     }
 
-    private void insertionReplace(String[] fromWords, String[] toWords) {
-        int matches = 0, difference = toWords.length - fromWords.length;
-        // loops through the array and counts matches
+    private int countMatches(String[] fromWords) {
+        int matches = 0;
         for (int i = 0; i < size(); i++) {
             if (wordsArray[i].equals(fromWords[0])) {
                 String[] slice = Arrays.copyOfRange(wordsArray, i, i + fromWords.length);
@@ -68,22 +67,40 @@ public class TheHistoryArray implements TheHistory {
                 }
             }
         }
-        String[] tempArray = new String[size() + (difference * matches)]; // creates new array with correct size
-        System.arraycopy(wordsArray, 0, tempArray, 0, size()); // copies initial items
-        int offset = 0;
-        // loops through the array a second time
+        return matches;
+    }
+
+    private int[] gatherMatchIndexes(String[] fromWords, int matches) {
+        int[] matchIndexes = new int[matches + 1];
+        matches = 0;
         for (int i = 0; i < size(); i++) {
             if (wordsArray[i].equals(fromWords[0])) {
                 String[] slice = Arrays.copyOfRange(wordsArray, i, i + fromWords.length);
                 if (Arrays.equals(slice, fromWords)) {
-                    // if there's a match copies the new words to the new array at the correct position
-                    System.arraycopy(toWords, 0, tempArray, i + offset, toWords.length);
-                    // copies the remaining elements from the original array to their new position in the new array
-                    System.arraycopy(wordsArray, i + fromWords.length, tempArray, i + toWords.length + offset, size() - i - fromWords.length);
-                    i += fromWords.length - 1; // makes the loop skip to the end of the matched elements
-                    offset += difference; // saves the amount of positions the elements need to be shifted each time when copying
+                    matchIndexes[matches++] = i;
+                    i += fromWords.length - 1;
                 }
             }
+        }
+        matchIndexes[matches] = size() - fromWords.length;
+        return matchIndexes;
+    }
+
+    private void insertionReplace(String[] fromWords, String[] toWords) {
+        int matches = countMatches(fromWords); // counts the number of matches
+        int[] matchIndexes = gatherMatchIndexes(fromWords, matches); // gathers the indexes of the matches
+        int difference = toWords.length - fromWords.length;
+        String[] tempArray = new String[size() + (difference * matches)]; // creates new array with correct size
+        // copies the items before first match index
+        System.arraycopy(wordsArray, 0, tempArray, 0, matchIndexes[0]);
+        int offset = 0;
+        // loops through the matches
+        for (int i = 0; i < matches; i++) {
+            // copies the new words to the match index
+            System.arraycopy(toWords, 0, tempArray, matchIndexes[i] + offset, toWords.length);
+            // copies the items from the original array between two match indexes (replaceable words excluded)
+            System.arraycopy(wordsArray, matchIndexes[i] + fromWords.length, tempArray, matchIndexes[i] + toWords.length + offset,  matchIndexes[i + 1] - matchIndexes[i]);
+            offset += difference; // saves the amount of index shifting needed for the next copying
         }
         wordsArray = tempArray; // sets the original array's reference to the new array
     }
